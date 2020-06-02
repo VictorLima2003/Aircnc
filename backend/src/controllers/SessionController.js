231c20933d4e2d3b741b7d89c1c33f2dbf347525
Victor Lima <victormamed2016@gmail.com>
2020-06-02 16:40:51 -0300

    first commit

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,25 @@
+const User = require('../models/User');
+
+//index, show, store, update, destroy
+
+module.exports = {
+    async index(req, res) {
+        const users = await User.find();
+
+        return res.json(users);
+    },
+
+    async store(req, res) {
+        const { email } = req.body;
+
+        let user = await User.findOne({ email });
+
+        if (!user) {
+            user = await User.create({ email })
+        }
+
+       // const user = await User.create({ email });
+
+        return res.json(user);
+    }
+}
